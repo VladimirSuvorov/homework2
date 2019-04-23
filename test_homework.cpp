ec6a383b3907953b1b9b76572cdd07f9123adf66
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-23 13:42:44 +0300

    Update test_homework.cpp

--- test_homework.cpp
+++ test_homework.cpp
@@ -1028,56 +1028,56 @@ public:
 TEST_F(HomeworkTest, Input_MUST_be_correct) { 
     auto ip_pool=read_ip4_addresses(input);
     //For short, only first and last 3 addresses are tested
-    ASSERT_STREQ((ip_pool[0].to_string().c_str(),"113.162.145.156"));
-    ASSERT_STREQ((ip_pool[1].to_string().c_str(),"157.39.22.224"));
-    ASSERT_STREQ((ip_pool[2].to_string().c_str(),"79.180.73.190"));
+    ASSERT_STREQ(ip_pool[0].to_string().c_str(),"113.162.145.156");
+    ASSERT_STREQ(ip_pool[1].to_string().c_str(),"157.39.22.224");
+    ASSERT_STREQ(ip_pool[2].to_string().c_str(),"79.180.73.190");
 
-    ASSERT_STREQ((ip_pool[(ip_pool.size()-3].to_string().c_str(),"67.183.105.87"));
-    ASSERT_STREQ((ip_pool[(ip_pool.size()-2].to_string().c_str(),"91.64.189.196"));
-    ASSERT_STREQ((ip_pool[(ip_pool.size()-1)].to_string().c_str(),"93.179.90.82"));
+    ASSERT_STREQ(ip_pool[(ip_pool.size()-3].to_string().c_str(),"67.183.105.87");
+    ASSERT_STREQ(ip_pool[(ip_pool.size()-2].to_string().c_str(),"91.64.189.196");
+    ASSERT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"93.179.90.82");
 }
 
 TEST_F(HomeworkTest, test_lexicographical_reverse) { 
     auto lexicographically_reversed = reverse_lexicographically_sort(read_ip4_addresses(input));
     //for this test it is assumed, that read_ip4_addresses is correct
-    EXPECT_STREQ((lexicographically_reversed[0].to_string().c_str(),"222.173.235.246"));
-    EXPECT_STREQ((lexicographically_reversed[1].to_string().c_str(),"222.130.177.64"));
-    EXPECT_STREQ((lexicographically_reversed[2].to_string().c_str(),"222.82.198.61"));
+    EXPECT_STREQ(lexicographically_reversed[0].to_string().c_str(),"222.173.235.246");
+    EXPECT_STREQ(lexicographically_reversed[1].to_string().c_str(),"222.130.177.64");
+    EXPECT_STREQ(lexicographically_reversed[2].to_string().c_str(),"222.82.198.61");
     
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-3].to_string().c_str(),"1.70.44.170"));
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-2].to_string().c_str(),"1.29.168.152"));
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-1)].to_string().c_str(),"1.1.234.8"));    
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-3].to_string().c_str(),"1.70.44.170");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-2].to_string().c_str(),"1.29.168.152");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"1.1.234.8");    
 }
 
 TEST_F(HomeworkTest, test_filtration_by_first_byte) { 
     auto filtered_by_first_byte = filter(read_ip4_addresses(input), 1);
     //for this test it is assumed, that read_ip4_addresses is correct
-    EXPECT_STREQ((lexicographically_reversed[0].to_string().c_str(),"1.231.69.33"));
-    EXPECT_STREQ((lexicographically_reversed[1].to_string().c_str(),"1.87.203.225"));
-    EXPECT_STREQ((lexicographically_reversed[2].to_string().c_str(),"1.70.44.170"));
-    EXPECT_STREQ((lexicographically_reversed[3].to_string().c_str(),"1.29.168.152"));
-    EXPECT_STREQ((lexicographically_reversed[4].to_string().c_str(),"1.1.234.8"));
+    EXPECT_STREQ(lexicographically_reversed[0].to_string().c_str(),"1.231.69.33");
+    EXPECT_STREQ(lexicographically_reversed[1].to_string().c_str(),"1.87.203.225");
+    EXPECT_STREQ(lexicographically_reversed[2].to_string().c_str(),"1.70.44.170");
+    EXPECT_STREQ(lexicographically_reversed[3].to_string().c_str(),"1.29.168.152");
+    EXPECT_STREQ(lexicographically_reversed[4].to_string().c_str(),"1.1.234.8");
 }
 
 TEST_F(HomeworkTest, test_filtration_by_first_and_second_bytes) { 
     auto filtered_by_first_and_second_bytes = filter(read_ip4_addresses(input), 46, 70); 
     //for this test it is assumed, that read_ip4_addresses is correct  
-    EXPECT_STREQ((lexicographically_reversed[0].to_string().c_str(),"46.70.225.39"));
-    EXPECT_STREQ((lexicographically_reversed[1].to_string().c_str(),"46.70.147.26"));
-    EXPECT_STREQ((lexicographically_reversed[2].to_string().c_str(),"46.70.113.73"));
-    EXPECT_STREQ((lexicographically_reversed[3].to_string().c_str(),"46.70.29.76"));
+    EXPECT_STREQ(lexicographically_reversed[0].to_string().c_str(),"46.70.225.39");
+    EXPECT_STREQ(lexicographically_reversed[1].to_string().c_str(),"46.70.147.26");
+    EXPECT_STREQ(lexicographically_reversed[2].to_string().c_str(),"46.70.113.73");
+    EXPECT_STREQ(lexicographically_reversed[3].to_string().c_str(),"46.70.29.76");
 }
 
 TEST_F(HomeworkTest, test_filtration_by_any_byte) { 
     auto filtered_by_any_byte = filter_any(read_ip4_addresses(input), 46);
     //for this test it is assumed, that read_ip4_addresses is correct
-    EXPECT_STREQ((lexicographically_reversed[0].to_string().c_str(),"186.204.34.46"));
-    EXPECT_STREQ((lexicographically_reversed[1].to_string().c_str(),"186.46.222.194"));
-    EXPECT_STREQ((lexicographically_reversed[2].to_string().c_str(),"185.46.87.231"));
+    EXPECT_STREQ(lexicographically_reversed[0].to_string().c_str(),"186.204.34.46");
+    EXPECT_STREQ(lexicographically_reversed[1].to_string().c_str(),"186.46.222.194");
+    EXPECT_STREQ(lexicographically_reversed[2].to_string().c_str(),"185.46.87.231");
     
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-3].to_string().c_str(),"46.49.43.85"));
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-2].to_string().c_str(),"39.46.86.85"));
-    EXPECT_STREQ((ip_pool[(ip_pool.size()-1)].to_string().c_str(),"5.189.203.46"));
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-3].to_string().c_str(),"46.49.43.85");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-2].to_string().c_str(),"39.46.86.85");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"5.189.203.46");
 }
 
 
