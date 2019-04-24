d9e3b74be6c581e476968dfd7165420c98d6536b
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-24 10:14:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]){
         auto ip_pool(read_ip4_addresses());    
 
         reverse_lexicographically_sort(ip_pool);
-        output(ip_pool);
+        output(decltype(ip_pool){ip_pool});
         output(filter(ip_pool, 1));
         output(filter(ip_pool, 46, 70));
         output(filter_any(ip_pool, 46));
