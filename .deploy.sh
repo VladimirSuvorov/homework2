d84f766ada1586e7ff4ee3c4a812826b5f277428
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-24 17:10:31 +0300

    Update .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -4,12 +4,20 @@
 # $2 - BINTRAY_API_KEY
 
 baseurl='https://api.bintray.com/content'
-project='if_filter'
+user='vsuvorov'
+project='otus-cpp'
 name='if_filter'
 version="0.0.$1"
-user='vsuvorov'
+package="${name}_${version}_amd64.deb"
 params=';deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1'
 
-package="${name}_${version}_amd64.deb"
 
 curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package$params"
+
+#curl -T helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb -uvladimirsuvorov:$BINTRAY_API_KEY 
+#"https://api.bintray.com/content/
+#vladimirsuvorov
+#/otus-cpp/
+#helloworld/$TRAVIS_BUILD_NUMBER/
+#helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb
+#;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1"
