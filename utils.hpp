c804e4b179a774a7ff67e31b938d599d0fa7da01
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-24 10:01:19 +0300

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -2,9 +2,6 @@
 
 #include <functional>
 #include <type_traits>
-//A thing, kinda a not fully featured concepts emulation...
-//see template defs of filter## functions
-#define REQUIRES(...) typename=std::enable_if_t<__VA_ARGS__>
 
 template<typename Container>
 using value_type_of = typename Container::value_type;
@@ -24,6 +21,9 @@ public:
 template<typename T>
 constexpr bool has_greater_predicate_v = has_greater_predicate<T>::value;
 
+template <typename Container,
+    typename = std::enable_if_t<has_greater_predicate_v<value_type_of<Container>>>>
+using WithValueSupportingGreatherPredicate = Container;
 
 template<typename T>
 struct has_left_shift_output{
@@ -37,3 +37,19 @@ public:
 };
 template<typename T>
 constexpr bool has_left_shift_output_v = has_left_shift_output<T>::value;
+
+template <typename Container,
+    typename = std::enable_if_t<has_left_shift_output_v<value_type_of<Container>>>>
+using WithValueHavingLeftShiftOutput = Container;
+
+
+template <typename Container,typename T, 
+    typename = std::enable_if_t<
+        std::is_same_v<
+            value_type_of<Container>, 
+            T>
+       >
+>
+using WithValueOfType = Container;
+
+
