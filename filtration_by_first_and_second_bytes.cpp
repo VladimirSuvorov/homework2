51f6c8d1123f559f0099f685130b0c76237fbbde
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:17:05 +0300

    Update filtration_by_first_and_second_bytes.cpp

--- filtration_by_first_and_second_bytes.cpp
+++ filtration_by_first_and_second_bytes.cpp
@@ -1,1 +1,11 @@
 #include "homework_test.hpp"
+
+TEST_F(HomeworkTest, test_filtration_by_first_and_second_bytes) {    
+    auto ip_pool=read_ip4_addresses(input);
+    auto filtered_by_first_and_second_bytes = filter(ip_pool, 46, 70); 
+    //for this test it is assumed, that read_ip4_addresses is correct  
+    EXPECT_STREQ(filtered_by_first_and_second_bytes[0].to_string().c_str(),"46.70.225.39");
+    EXPECT_STREQ(filtered_by_first_and_second_bytes[1].to_string().c_str(),"46.70.147.26");
+    EXPECT_STREQ(filtered_by_first_and_second_bytes[2].to_string().c_str(),"46.70.113.73");
+    EXPECT_STREQ(filtered_by_first_and_second_bytes[3].to_string().c_str(),"46.70.29.76");
+}
