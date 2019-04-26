7c35570ee86a8963319987b4ed314cbcfff10e0b
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 16:53:52 +0300

    Update filtration_by_any_byte.cpp

--- filtration_by_any_byte.cpp
+++ filtration_by_any_byte.cpp
@@ -4,7 +4,9 @@ TEST_F(HomeworkTest, test_filtration_by_any_byte) {
     auto ip_pool=read_ip4_addresses(input);
     reverse_lexicographically_sort(ip_pool);
     auto filtered_by_any_byte = filter_any(ip_pool, 46);
-    //for this test it is assumed, that read_ip4_addresses is correct
+    
+    ASSERT_EQ(filtered_by_any_byte.size(), 34);
+    
     EXPECT_STREQ(filtered_by_any_byte[0].to_string().c_str(),"186.204.34.46");
     EXPECT_STREQ(filtered_by_any_byte[1].to_string().c_str(),"186.46.222.194");
     EXPECT_STREQ(filtered_by_any_byte[2].to_string().c_str(),"185.46.87.231");
