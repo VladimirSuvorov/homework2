c73f5e7b487ad30543870c4db3f7a6882a5e91f3
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:15:37 +0300

    Update lexicographical_reverse.cpp

--- lexicographical_reverse.cpp
+++ lexicographical_reverse.cpp
@@ -1,1 +1,15 @@
 #include "homework_test.hpp"
+
+TEST_F(HomeworkTest, test_lexicographical_reverse) { 
+    auto ip_pool=read_ip4_addresses(input);
+    reverse_lexicographically_sort(ip_pool);
+   
+    //for this test it is assumed, that read_ip4_addresses is correct
+    EXPECT_STREQ(ip_pool[0].to_string().c_str(),"222.173.235.246");
+    EXPECT_STREQ(ip_pool[1].to_string().c_str(),"222.130.177.64");
+    EXPECT_STREQ(ip_pool[2].to_string().c_str(),"222.82.198.61");
+    
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-3)].to_string().c_str(),"1.70.44.170");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-2)].to_string().c_str(),"1.29.168.152");
+    EXPECT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"1.1.234.8");    
+}
