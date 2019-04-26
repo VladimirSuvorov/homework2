15db0be2a10ae35104fe73d52d37a752b264ddfe
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:16:50 +0300

    Update input_test.cpp

--- input_test.cpp
+++ input_test.cpp
@@ -19,16 +19,6 @@ TEST_F(HomeworkTest, Input_MUST_be_correct) {
 }
 
 
-TEST_F(HomeworkTest, test_filtration_by_first_and_second_bytes) {    
-    auto ip_pool=read_ip4_addresses(input);
-    auto filtered_by_first_and_second_bytes = filter(ip_pool, 46, 70); 
-    //for this test it is assumed, that read_ip4_addresses is correct  
-    EXPECT_STREQ(filtered_by_first_and_second_bytes[0].to_string().c_str(),"46.70.225.39");
-    EXPECT_STREQ(filtered_by_first_and_second_bytes[1].to_string().c_str(),"46.70.147.26");
-    EXPECT_STREQ(filtered_by_first_and_second_bytes[2].to_string().c_str(),"46.70.113.73");
-    EXPECT_STREQ(filtered_by_first_and_second_bytes[3].to_string().c_str(),"46.70.29.76");
-}
-
 TEST_F(HomeworkTest, test_filtration_by_any_byte) {    
     auto ip_pool=read_ip4_addresses(input);
     auto filtered_by_any_byte = filter_any(ip_pool, 46);
