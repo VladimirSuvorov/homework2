4d607576a43eab4eb4a072d52c59339f4348056e
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:15:15 +0300

    Update input_test.cpp

--- input_test.cpp
+++ input_test.cpp
@@ -18,19 +18,6 @@ TEST_F(HomeworkTest, Input_MUST_be_correct) {
     ASSERT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"93.179.90.82");
 }
 
-TEST_F(HomeworkTest, test_lexicographical_reverse) { 
-    auto ip_pool=read_ip4_addresses(input);
-    reverse_lexicographically_sort(ip_pool);
-   
-    //for this test it is assumed, that read_ip4_addresses is correct
-    EXPECT_STREQ(ip_pool[0].to_string().c_str(),"222.173.235.246");
-    EXPECT_STREQ(ip_pool[1].to_string().c_str(),"222.130.177.64");
-    EXPECT_STREQ(ip_pool[2].to_string().c_str(),"222.82.198.61");
-    
-    EXPECT_STREQ(ip_pool[(ip_pool.size()-3)].to_string().c_str(),"1.70.44.170");
-    EXPECT_STREQ(ip_pool[(ip_pool.size()-2)].to_string().c_str(),"1.29.168.152");
-    EXPECT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"1.1.234.8");    
-}
 
 TEST_F(HomeworkTest, test_filtration_by_first_byte) {    
     auto ip_pool=read_ip4_addresses(input);
