f1235676a5227e914a3d24cab232ad3be2652486
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:18:18 +0300

    Update input_test.cpp

--- input_test.cpp
+++ input_test.cpp
@@ -1,8 +1,4 @@
 #include "homework_test.hpp"
-//All tests, except a following input test, 
-//are using EXPECT and only the input test is using ASSERT,
-//because wrong input leads to wrong results of other tests.
-
 TEST_F(HomeworkTest, Input_MUST_be_correct) {    
     auto ip_pool=read_ip4_addresses(input);
     //For short, only first and last 3 addresses are tested
