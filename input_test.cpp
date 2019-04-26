906795f4c393a958647fd73fc0f2d53159d87082
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 15:17:34 +0300

    Update input_test.cpp

--- input_test.cpp
+++ input_test.cpp
@@ -1,7 +1,4 @@
 #include "homework_test.hpp"
-
-
-/*
 //All tests, except a following input test, 
 //are using EXPECT and only the input test is using ASSERT,
 //because wrong input leads to wrong results of other tests.
@@ -17,19 +14,3 @@ TEST_F(HomeworkTest, Input_MUST_be_correct) {
     ASSERT_STREQ(ip_pool[(ip_pool.size()-2)].to_string().c_str(),"91.64.189.196");
     ASSERT_STREQ(ip_pool[(ip_pool.size()-1)].to_string().c_str(),"93.179.90.82");
 }
-
-
-TEST_F(HomeworkTest, test_filtration_by_any_byte) {    
-    auto ip_pool=read_ip4_addresses(input);
-    auto filtered_by_any_byte = filter_any(ip_pool, 46);
-    //for this test it is assumed, that read_ip4_addresses is correct
-    EXPECT_STREQ(filtered_by_any_byte[0].to_string().c_str(),"186.204.34.46");
-    EXPECT_STREQ(filtered_by_any_byte[1].to_string().c_str(),"186.46.222.194");
-    EXPECT_STREQ(filtered_by_any_byte[2].to_string().c_str(),"185.46.87.231");
-    
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-3)].to_string().c_str(),"46.49.43.85");
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-2)].to_string().c_str(),"39.46.86.85");
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-1)].to_string().c_str(),"5.189.203.46");
-}
-*/
-
