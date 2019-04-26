646a0447a61db7291b2ba616cb6782f0fdbcdfd1
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 16:45:32 +0300

    Update homework.hpp

--- homework.hpp
+++ homework.hpp
@@ -19,57 +19,60 @@ ip_pool_t read_ip4_addresses(std::istream& in=std::cin){
     using namespace std;     
     ip_pool_t ip_pool;
     for(string ip_string; getline(in, ip_string);) //unsafe const_cast for this case, BUT! - it workes fine/
-        ip_pool.emplace_back(std::move(bai::address_v4::from_string(ip_string.substr(0,ip_string.find_first_of(" \t\n")))));
+        ip_pool.emplace_back(move(bai::address_v4::from_string(ip_string.substr(0, ip_string.find_first_of(" \t\n")))));
     return ip_pool;
 }
 
 
 
-//Applies any STL-compatible container, 
-// with value_type having greater comparsion operator>
+//Applies any STL-compatible container, with a value_type having greater comparsion operator>
 template<typename Container>
-void reverse_lexicographically_sort(WithValueSupportingGreatherPredicate<Container>& c){
-    std::sort(std::begin(c), std::end(c), std::greater<value_type_of<Container>>());    
+void reverse_lexicographically_sort(WithValueSupportingGreatherPredicate<Container>& ip_addresses_container){
+    using namespace std;
+    sort(begin(ip_addresses_container), end(ip_addresses_container), greater<value_type_of<Container>>());    
 }
+
+namespace{//filter function family implementation detatils
+    template<typename Container, typename UnaryPredicate>
+    Container filter_implementation(const Container& ip_addresses_container, UnaryPredicate unary_predicate){
+        using namespace std;
+        Container result_ip_addresses_container; 
+        remove_copy_if(cbegin(ip_addresses_container), cend(ip_addresses_container), begin(result_ip_addresses_container), 
+        [&unary_predicate](auto&& ip_address){
+            return (!unary_predicate(forward<remove_reference_t<decltype(ip_address)>>(ip_address)));
+        });
+        return result_ip_addresses_container;
+    }
+}//detatils
 
 //Any filter## function:
 //1) applies any STL-compatible container, with boost::asio::ip::address_v4 as a value_type
 //2) returns a copy of an input container, filled with filtered IPs
-namespace{
-    template<typename Container, typename UnaryPredicate>
-    Container filter_implementation(const Container& c, UnaryPredicate unary_predicate){
-        Container r;//NOTE: unary_predicate is inverted in the lambda, cos we need to invert the condition we're looking for due to use of remove_copy_if's result. 
-        std::remove_copy_if(std::cbegin(c), std::cend(c), std::begin(r), [&unary_predicate](auto&& v){
-            return (!unary_predicate(std::forward<decltype(v)>(v)));
-        });
-        return r;
-    }
-}
-
 template<typename Container>
-Container filter(const WithValueOfType<Container,bai::address_v4>& c, uint8_t first_byte){
-    return filter_implementation(c, [first_byte](auto &&v){
-        return (v.to_bytes()[0]==first_byte);
+Container filter(const WithValueOfType<Container,bai::address_v4>& ip_addresses_container, uint8_t first_byte){
+    return filter_implementation(ip_addresses_container, [first_byte](auto &&ip_address){
+        return (ip_address.to_bytes()[0]==first_byte);
     });
 }
 template<typename Container>
-Container filter(const WithValueOfType<Container,bai::address_v4>& c, uint8_t first_byte, uint8_t second_byte){
-    return filter_implementation(c, [first_byte, second_byte](auto &&v){
-        auto ip_bytes = v.to_bytes();
+Container filter(const WithValueOfType<Container,bai::address_v4>& ip_addresses_container, uint8_t first_byte, uint8_t second_byte){
+    return filter_implementation(ip_addresses_container, [first_byte, second_byte](auto &&ip_address){
+        auto ip_bytes = ip_address.to_bytes();
         return ((ip_bytes[0]==first_byte)&&(ip_bytes[1]==second_byte));
     });
 }
 template<typename Container>
-Container filter_any(const WithValueOfType<Container,bai::address_v4>& c, uint8_t any_byte){
-    return filter_implementation(c, [any_byte](auto &&v){
-        auto ip_bytes = v.to_bytes();
-        return (std::cend(ip_bytes)!=std::find(std::cbegin(ip_bytes), std::cend(ip_bytes), any_byte));
+Container filter_any(const WithValueOfType<Container,bai::address_v4>& ip_addresses_container, uint8_t any_byte){
+    return filter_implementation(ip_addresses_container, [any_byte](auto &&ip_address){        
+        using namespace std;
+        auto ip_bytes = ip_address.to_bytes();
+        return (cend(ip_bytes)!=find(cbegin(ip_bytes), cend(ip_bytes), any_byte));
     });
 }
 
-//Applies any STL-compatible container,
-// with value_type having stream output operator<< overload 
+//Applies any STL-compatible container, with a value_type having stream output operator<< overload 
 template<typename Container>
-void output(WithValueHavingLeftShiftOutput<Container>&& c, std::ostream& out=std::cout){
-    std::for_each(std::cbegin(c), std::cend(c), [&out](auto &&v){ out<<v<<std::endl; });
+void output(WithValueHavingLeftShiftOutput<Container>&& ip_addresses_container, std::ostream& out=std::cout){
+    using namespace std;
+    for_each(cbegin(ip_addresses_container), cend(ip_addresses_container), [&out](auto &&v){ out<<v<<std::endl; });
 }
