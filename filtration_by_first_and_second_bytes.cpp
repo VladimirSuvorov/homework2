71e580610e2e2ec1b3159b22012e4e4f9bebb90c
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 16:52:50 +0300

    Update filtration_by_first_and_second_bytes.cpp

--- filtration_by_first_and_second_bytes.cpp
+++ filtration_by_first_and_second_bytes.cpp
@@ -4,7 +4,9 @@ TEST_F(HomeworkTest, test_filtration_by_first_and_second_bytes) {
     auto ip_pool=read_ip4_addresses(input);
     reverse_lexicographically_sort(ip_pool);
     auto filtered_by_first_and_second_bytes = filter(ip_pool, 46, 70); 
-    //for this test it is assumed, that read_ip4_addresses is correct  
+    
+    ASSERT_EQ(filtered_by_first_and_second_bytes.size(), 4);
+    
     EXPECT_STREQ(filtered_by_first_and_second_bytes[0].to_string().c_str(),"46.70.225.39");
     EXPECT_STREQ(filtered_by_first_and_second_bytes[1].to_string().c_str(),"46.70.147.26");
     EXPECT_STREQ(filtered_by_first_and_second_bytes[2].to_string().c_str(),"46.70.113.73");
