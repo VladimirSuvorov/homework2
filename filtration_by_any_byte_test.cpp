126164528469d8ede9501ae534f3536207554a1e
VladimirSuvorov <32199420+VladimirSuvorov@users.noreply.github.com>
2019-04-26 18:45:55 +0300

    Update filtration_by_any_byte_test.cpp

--- filtration_by_any_byte_test.cpp
+++ filtration_by_any_byte_test.cpp
@@ -2,8 +2,8 @@
 
 TEST_F(HomeworkTest, test_filtration_by_any_byte) {    
     auto ip_pool=read_ip4_addresses(input);
-    auto filtered_by_any_byte = filter_any(ip_pool, 46);
     reverse_lexicographically_sort(filtered_by_any_byte);
+    auto filtered_by_any_byte = filter_any(ip_pool, 46);
     
     ASSERT_EQ(filtered_by_any_byte.size(), 34);
     
@@ -11,7 +11,7 @@ TEST_F(HomeworkTest, test_filtration_by_any_byte) {
     EXPECT_STREQ(filtered_by_any_byte[1].to_string().c_str(),"186.46.222.194");
     EXPECT_STREQ(filtered_by_any_byte[2].to_string().c_str(),"185.46.87.231");
     
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-3)].to_string().c_str(),"46.49.43.85");
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-2)].to_string().c_str(),"39.46.86.85");
-    EXPECT_STREQ(filtered_by_any_byte[(ip_pool.size()-1)].to_string().c_str(),"5.189.203.46");
+    EXPECT_STREQ(filtered_by_any_byte[(filtered_by_any_byte.size()-3)].to_string().c_str(),"46.49.43.85");
+    EXPECT_STREQ(filtered_by_any_byte[(filtered_by_any_byte.size()-2)].to_string().c_str(),"39.46.86.85");
+    EXPECT_STREQ(filtered_by_any_byte[(filtered_by_any_byte.size()-1)].to_string().c_str(),"5.189.203.46");
 }
